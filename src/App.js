3d5dcd5a8b5434cb677f44984ea1d835617ab6ce
Miika Kulmala <kulmmii@gmail.com>
2022-10-03 07:39:00 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./styles.css";
 import TopNav from "./Components/TopNav";
 import Map from "./Components/Map";
 import BottomNav from "./Components/BottomNav";
+import ServiceProviderCard from "./Components/ServiceProviderCard";
 
 export default function App() {
   return (
     <div>
       <TopNav />
       <Map />
+      <ServiceProviderCard />
       <BottomNav />
     </div>
   );
---
+++ src/Components/ServiceProviderCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ServiceProviderCard() {
+  return (
+    <nav className="ServiceProviderCard">
+      <div> ServiceProviderCard </div>
+    </nav>
+  );
+}
